7cf3c57b2736cd6aaa065cfb3bc428ea03e18a20
Tamizhvendan S <tamizh88@gmail.com>
2013-04-27 16:51:12 +0530

    Fixing migration issues

--- plugins/tag_cloud.rb
+++ plugins/tag_cloud.rb
@@ -1,111 +1,116 @@
-# tag_cloud.rb
-#
-# Copyright (C) 2011 Anurag Priyam - MIT License
+# encoding: utf-8
+
+# Tag Cloud for Octopress
+# =======================
+# 
+# Description:
+# ------------
+# Easy output tag cloud and category list.
+# 
+# Syntax:
+# -------
+#     {% tag_cloud [counter:true] %}
+#     {% category_list [counter:true] %}
+# 
+# Example:
+# --------
+# In some template files, you can add the following markups.
+# 
+# ### source/_includes/custom/asides/tag_cloud.html ###
+# 
+#     <section>
+#       <h1>Tag Cloud</h1>
+#         <span id="tag-cloud">{% tag_cloud %}</span>
+#     </section>
+# 
+# ### source/_includes/custom/asides/category_list.html ###
+# 
+#     <section>
+#       <h1>Categories</h1>
+#         <ul id="category-list">{% category_list counter:true %}</ul>
+#     </section>
+# 
+# Notes:
+# ------
+# Be sure to insert above template files into `default_asides` array in `_config.yml`.
+# And also you can define styles for 'tag-cloud' or 'category-list' in a `.scss` file.
+# (ex: `sass/custom/_styles.scss`)
+# 
+# Licence:
+# --------
+# Distributed under the [MIT License][MIT].
+# 
+# [MIT]: http://www.opensource.org/licenses/mit-license.php
+# 
+require 'stringex'
 
 module Jekyll
 
-  # Jekyll plugin to generate tag clouds.
-  #
-  # The plugin defines a `tag_cloud` tag that is rendered by Liquid into a tag
-  # cloud:
-  #
-  #     <div class='cloud'>
-  #         {% tag_cloud %}
-  #     </div>
-  #
-  # The tag cloud itself is a collection of anchor tags, styled dynamically
-  # with the `font-size` CSS property. The range of values, and unit to use for
-  # `font-size` can be specified with a very simple syntax:
-  #
-  #     {% tag_cloud font-size: 16 - 28px %}
-  #
-  # The output is automatically formatted to use the same number of decimal
-  # places as used in the argument:
-  #
-  #     {% tag_cloud font-size: 0.8  - 1.8em  %}  # => 1
-  #     {% tag_cloud font-size: 0.80 - 1.80em %}  # => 2
-  #
-  # Tags that have been used less than a certain number of times can be
-  # filtered out from the tag cloud with the optional `threshold` parameter:
-  #
-  #     {% tag_cloud threshold: 2%}
-  #
-  # Both the parameters can be easily clubbed together:
-  #
-  #     {% tag_cloud font-size: 50 - 150%, threshold: 2%}
-  #
-  # The plugin randomizes the order of tags every time the cloud is generated.
   class TagCloud < Liquid::Tag
-    safe = true
 
-    # tag cloud variables - these are setup in `initialize`
-    attr_reader :size_min, :size_max, :precision, :unit, :threshold
-
-    def initialize(name, params, tokens)
-      # initialize default values
-      @size_min, @size_max, @precision, @unit = 70, 170, 0, '%'
-      @threshold                              = 1
-
-      # process parameters
-      @params = Hash[*params.split(/(?:: *)|(?:, *)/)]
-      process_font_size(@params['font-size'])
-      process_threshold(@params['threshold'])
-
+    def initialize(tag_name, markup, tokens)
+      @opts = {}
+      if markup.strip =~ /\s*counter:(\w+)/i
+        @opts['counter'] = ($1 == 'true')
+        markup = markup.strip.sub(/counter:\w+/i,'')
+      end
       super
     end
 
     def render(context)
-      # get category directory of category page
+      lists = {}
+      max, min = 1, 1
       config = context.registers[:site].config
-      dir = config['root'] + config['category_dir']
-
-      # get an Array of [tag name, tag count] pairs
-      count = context.registers[:site].categories.map do |name, posts|
-        [name, posts.count] if posts.count >= threshold
+      category_dir = config['root'] + config['category_dir'] + '/'
+      categories = context.registers[:site].categories
+      categories.keys.sort_by{ |str| str.downcase }.each do |category|
+        count = categories[category].count
+        lists[category] = count
+        max = count if count > max
       end
 
-      # clear nils if any
-      count.compact!
-
-      # get the minimum, and maximum tag count
-      min, max = count.map(&:last).minmax
-
-      # map: [[tag name, tag count]] -> [[tag name, tag weight]]
-      weight = count.map do |name, count|
-        # logarithmic distribution
-        weight = (Math.log(count) - Math.log(min))/(Math.log(max) - Math.log(min))
-        [name, weight]
+      html = ''
+      lists.each do | category, counter |
+        url = category_dir + category.to_url
+        style = "font-size: #{100 + (60 * Float(counter)/max)}%"
+        html << "<a href='#{url}' style='#{style}'>#{category}"
+        if @opts['counter']
+          html << "(#{categories[category].count})"
+        end
+        html << "</a> "
       end
-
-      # shuffle the [tag name, tag weight] pairs
-      weight.sort_by! { rand }
-
-      # reduce the Array of [tag name, tag weight] pairs to HTML
-      weight.reduce("") do |html, tag|
-        name, weight = tag
-        size = size_min + ((size_max - size_min) * weight).to_f
-        size = sprintf("%.#{@precision}f", size)
-        html << "<a style='font-size: #{size}#{unit}' href='#{dir}/#{name.to_url}/'>#{name}</a>\n"
-      end
-    end
-
-    private
-
-    def process_font_size(param)
-      /(\d*\.{0,1}(\d*)) *- *(\d*\.{0,1}(\d*)) *(%|em|px)/.match(param) do |m|
-        @size_min  = m[1].to_f
-        @size_max  = m[3].to_f
-        @precision = [m[2].size, m[4].size].max
-        @unit      = m[5]
-      end
-    end
-
-    def process_threshold(param)
-      /\d*/.match(param) do |m|
-        @threshold = m[0].to_i
-      end
+      html
     end
   end
+
+  class CategoryList < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      @opts = {}
+      if markup.strip =~ /\s*counter:(\w+)/i
+        @opts['counter'] = ($1 == 'true')
+        markup = markup.strip.sub(/counter:\w+/i,'')
+      end
+      super
+    end
+
+    def render(context)
+      html = ""
+      config = context.registers[:site].config
+      category_dir = config['category_dir']
+      categories = context.registers[:site].categories
+      categories.keys.sort_by{ |str| str.downcase }.each do |category|
+        html << "<li><a href='/#{category_dir}/#{category.to_url}/'>#{category}"
+        if @opts['counter']
+          html << " (#{categories[category].count})"
+        end
+        html << "</a></li>"
+      end
+      html
+    end
+  end
+
 end
 
 Liquid::Template.register_tag('tag_cloud', Jekyll::TagCloud)
+Liquid::Template.register_tag('category_list', Jekyll::CategoryList)
